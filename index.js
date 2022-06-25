ca600b849f120b61b603b87fca2d6cf6e3bee86f
Teo Wei Ming <teoweiming@Teos-MacBook-Pro.local>
2022-06-25 22:33:05 +0800

    revert back to standalone backend

--- index.js
+++ index.js
@@ -40,20 +40,6 @@ app.get("/api/hello", (req, res) => {
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 
-
-app.use(express.static(path.join(__dirname, "./tracktogether-frontend/build")));
-
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./tracktogether-frontend/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
-});
-
 app.listen(port, () => {
   console.log(`Server started on ${port}...`);
 });
